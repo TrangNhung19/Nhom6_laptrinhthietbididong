92efd4b704a33dc89ff2c3bb73012276fbde4b82
TrangNhung19 <94165318+TrangNhung19@users.noreply.github.com>
2023-03-23 19:30:14 +0700

    todoList

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:calculator/calculator_screen.dart';
-void main() {
+import 'package:hive_flutter/hive_flutter.dart';
+import 'pages/home_page.dart';
+
+void main() async {
+  // init the hive
+  await Hive.initFlutter();
+
+  // open a box
+  var box = await Hive.openBox('mybox');
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Calculator(),
+      home: HomePage(),
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
     );
   }
 }
-
-
